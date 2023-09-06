9f3f0577a2fd4501ab252c82b8f78be43a3cb063
Michel Souza <michelsouza.contato@icloud.com>
2023-09-05 21:17:50 -0300

    atualização tela de Cadastro

--- screens/CadastroScreen.js
+++ screens/CadastroScreen.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
 
 
-export default function CadastroScreen() {
+export default function  CadastroScreen() {
   return (
     <View style={styles.container}>
         <Text>Cadastro</Text>
         <StatusBar style="light" />
+        <View style={styles.box}>
+        <View style={styles.boxInfo}></View>
+        </View>
     </View>
   );
 }
@@ -14,11 +17,22 @@ export default function CadastroScreen() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#691030',
+      backgroundColor: '#FFF',
       alignItems: 'center',
-      justifyContent: 'center',
-      padding: 53,
-      gap: 24,
+      justifyContent: 'center'
     },
-  });
+    box: {
+      backgroundColor: '#691030',
+      borderRadius: 32,
+      height: 606,
+      width: 322,
+      padding: 46
+    },
+    boxInfo:{
+      backgroundColor:"#E4E2E2",
+      padding:19,
+      width: "100%",
+     
+    }
+});
   
